55afbf34f00f47343af9c4f85117065f9081df44
Xilin Chen <bismuthchan11@gmail.com>
2023-10-23 11:32:36 +0100

    Finished course task11

--- group.py
+++ group.py
@@ -3,10 +3,29 @@
 # Your code to go here...
 
 
-Jill={"name":"Jill","age":26,"Job":"Biologist","Relation":{"Zalika":"friend","John":"partner"}}
-Zalika={"name":"Zalika","age":28,"Job":"artist","Relation":{"Jill":"friend","Nash":"renter"}}
-John={"name":"John","age":27,"Job":"writer","Relation":{"Jill":"partner","Nash":"uncle"}}
-Nash={"name":"Nash","age":34,"Job":"chef","Relation":{"John":"cousin","Zalika":"landlord"}}
+Jill={"name":"Jill",
+      "age":26,
+      "Job":"Biologist",
+      "Relation":{"Zalika":"friend", "John":"partner"}
+      }
+
+Zalika={"name":"Zalika",
+        "age":28,
+        "Job":"artist",
+        "Relation":{"Jill":"friend", "Nash":"renter"}
+        }
+
+John={"name":"John",
+      "age":27,
+      "Job":"writer",
+      "Relation":{"Jill":"partner","Nash":"uncle"}
+      }
+
+Nash={"name":"Nash",
+      "age":34,
+      "Job":"chef",
+      "Relation":{"John":"cousin","Zalika":"landlord"}
+      }
 
 
 def forget(person1,person2):
@@ -35,17 +54,55 @@ my_group.append(Jill)
 my_group.append(Zalika)
 my_group.append(John)
 my_group.append(Nash)
-print(my_group)
 
-forget(Jill,Zalika)
-print(Jill)
-print(Zalika)
+#forget(Jill,Zalika)
+#print(Jill)
+#print(Zalika)
 
 Dokolo = add_person("Dokolo", 24, "student", {})
 my_group.append(Dokolo)
-print(Dokolo)
+#print(Dokolo)
 
 print(average_age(my_group))
 
+def max_age(group_list):
+    max_age=0
+    for i in group_list:
+        if i["age"] >= max_age:
+            max_age = i["age"]
+    
+    return max_age
+print(max_age(my_group))
 
+def mean_relation(group_list):
+    total_relation=0
+    total_person=0
+    for i in group_list:
+        total_relation += len(i['Relation'])
+        total_person +=1
+    
+    return total_relation / total_person
+print(mean_relation(my_group))
 
+def max_age_relation(group_list):
+    max_age=0
+    max_name=""
+    for i in group_list:
+        if len(i["Relation"]) >= 1:
+            if i["age"] >= max_age:
+                max_age = i["age"]
+                max_name = i["name"]
+    return (max_age, max_name)
+print(max_age_relation(my_group))
+
+def max_age_name(group_list):
+    max_age=0
+    max_name=""
+    for i in group_list:
+        #print(i["Relation"])
+        if "friend" in i["Relation"].values():
+            if i["age"] >= max_age:
+                max_age = i["age"]
+                max_name = i["name"]
+    return (max_age, max_name)
+print(max_age_name(my_group))
